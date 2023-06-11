e5681372bce42970ce6a486bdc7216c6a63feffc
Abdesselam62 <slamusdr@hotmail.com>
2023-06-11 19:18:39 +0200

    Update eventListener

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,30 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Sélection des flèches gauche et droite
+const arrowLeft = document.querySelector('.arrow_left')
+const arrowRight = document.querySelector('.arrow_right')
+
+// Ajout des événements clic sur la flèche gauche et droite
+arrowLeft.addEventListener ('click',() => {ChangeSlide(-1)})
+arrowRight.addEventListener ('click',() => {ChangeSlide(1)})
+
+// Sélection de l'image de la bannière et du titre
+const bannerImage = document.querySelector('.banner-img')
+const bannerTitle = document.querySelector ('#banner p')
+
+// Initialisation du compteur
+let numero = 0
+
+ // Fonction pour mettre à jour l'image de la bannière et le titre
+function ChangeSlide(sens) {
+	
+	numero += sens
+	if (numero > slides.length - 1 )
+	numero = 0
+	if (numero < 0)
+	numero = slides.length - 1
+	bannerImage.src = "./assets/images/slideshow/" + slides[numero].image
+	bannerTitle.innerHTML = slides[numero].tagLine
+}
